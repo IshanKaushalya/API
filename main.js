ad72c5af97e4dd5a39c94a5da51bb49a54b3c9b1
Ishan Kaushalya <ikishankaushalya@gmail.com>
2024-04-05 18:48:35 +0530

    add cors package to working cross platforms.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const sql = require("mssql");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 const config = {
   user: "ikadmin",
@@ -24,28 +26,8 @@ sql
     console.error("Database connection failed: ", err);
   });
 
-//Endpoint to get all data
-app.get("/weather", async (req, res) => {
-  try {
-    // Connect to the database
-    await sql.connect(config);
-
-    // Query to select all data from the weather table
-    const result = await sql.query`SELECT * FROM districts`;
-
-    // Close the database connection
-    await sql.close();
-
-    // Send the data as JSON in the response
-    res.json(result.recordset);
-  } catch (error) {
-    console.error("Error fetching data:", error.message);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
 // Endpoint to fetch data from the weather table based on district
-app.get("/weather/:district", async (req, res) => {
+app.get("/district/:name", async (req, res) => {
   const district = req.params.district;
   try {
     // Connect to the database
@@ -65,10 +47,6 @@ app.get("/weather/:district", async (req, res) => {
     res.status(500).send("Internal Server Error");
   }
 });
-
-// app.get('/', (req, res) => {
-//   res.send('Hello, World!');
-// });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
